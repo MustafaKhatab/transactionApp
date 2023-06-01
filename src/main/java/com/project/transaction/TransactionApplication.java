469619ce6b3cd219fcf5950a8502be39f6fb1fab
Moemen ashraf <ahmedashraf8828@gmail.com>
2023-06-02 01:38:12 +0200

    logger

--- src/main/java/com/project/transaction/TransactionApplication.java
+++ src/main/java/com/project/transaction/TransactionApplication.java
@@ -2,10 +2,24 @@ package com.project.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
+@RestController
 public class TransactionApplication {
+	public static final Logger logger = LoggerFactory.getLogger(TransactionApplication.class);
+	@RequestMapping("/")
+	public String index() {
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
 
+		return "YO! Check out the Logs to see the output...";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionApplication.class, args);
 	}
